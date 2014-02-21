cbe40d69a43ca1e694385e2ea29ff56f25a54888
rajinir <rajini_ram@dell.com>
2014-02-21 11:07:05 -0600

    Handle databases for services

--- chef/cookbooks/database-custom/recipes/default.rb
+++ chef/cookbooks/database-custom/recipes/default.rb
@@ -14,8 +14,31 @@
 #
 
 node.override['build_essential']['compiletime'] = false
-node.override['openstack']['endpoints']['db']['host'] = node.address( "admin", IP::IP4 ).addr
+
+db_bind_address = node.address( "admin", IP::IP4 ).addr
+node.override['openstack']['endpoints']['db']['host'] = db_bind_address
+node.override['openstack']['db']['compute']['host'] = db_bind_address
+node.override['openstack']['db']['identity']['host'] = db_bind_address
+node.override['openstack']['db']['network']['host'] = db_bind_address
+node.override['openstack']['db']['image']['host'] = db_bind_address
+node.override['openstack']['db']['dashboard']['host'] = db_bind_address
+node.override['openstack']['db']['metering']['host'] = db_bind_address
+node.override['openstack']['db']['block-storage']['host'] = db_bind_address
+node.override['openstack']['db']['orchestration']['host'] = db_bind_address
+node.override['mysql']['root_network_acl'] =  db_bind_address
+
 
 package 'make' do
   action :nothing
 end.run_action(:install)
+
+node.default['openstack']['secret']['key_path'] = '/var/chef/data_bags/openstack_data_bag_secret'
+
+node.default['openstack']['db']['identity']['username'] = node['crowbar']['db']['identity']['username']
+node.default['openstack']['db']['compute']['username'] = node['crowbar']['db']['compute']['username']
+node.default['openstack']['db']['image']['username'] = node['crowbar']['db']['image']['username']
+node.default['openstack']['db']['network']['username'] = node['crowbar']['db']['network']['username']
+node.default['openstack']['db']['dashboard']['username'] = node['crowbar']['db']['dashboard']['username']
+node.default['openstack']['db']['metering']['username'] = node['crowbar']['db']['metering']['username']
+node.default['openstack']['db']['block-storage']['username'] = node['crowbar']['db']['block-storage']['username']
+node.default['openstack']['db']['orchestration']['username'] = node['crowbar']['db']['orchestration']['username']
--- chef/roles/database-server.rb
+++ chef/roles/database-server.rb
@@ -3,7 +3,8 @@ description "Currently MySQL Server (non-ha)"
 run_list(
     "role[os-base]",
     "recipe[database-custom]",
-    "recipe[openstack-ops-database::server]"
+    "recipe[openstack-ops-database::server]",
+    "recipe[openstack-ops-database::openstack-db]"
 )
 default_attributes()
 override_attributes()
